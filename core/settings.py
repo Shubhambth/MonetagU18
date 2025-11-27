1703c625eaaabd48d4bf41971680bb80e421a4ba
Shubham Singh <164128999+Shubhambth@users.noreply.github.com>
2025-11-27 13:18:14 +0000

    v3

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure--)pvprhdf$jmlnv2h*8@7j9b!3!!q2hkjv&hfd5vgeggxuu$^1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
